6510bff2bd565c93ff994a11d372a2b8db242615
Reid Shea <reidshea3@gmail.com>
2022-06-06 16:03:38 -0400

    working mobile navbar burger dropdown

--- src/app.js
+++ src/app.js
@@ -9,7 +9,38 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// ------------------------ NAVbar logo animation ------------------------
+// ----------------- NAVbar logo animation ------------------
+
+const navbarToWhite = () => {
+  gsap.to(".st0", { fill: "black" });
+  gsap.to(".navbar", {
+    color: "black",
+    backgroundColor: "white",
+  });
+  gsap.to(".BURGERBAR", {
+    backgroundColor: "black",
+  });
+  gsap.to(".MOBILE-MENU", {
+    color: "black",
+    backgroundColor: "white",
+  });
+};
+
+const navbarToTransparent = () => {
+  gsap.to(".st0", { fill: "white" });
+  gsap.to(".navbar", {
+    color: "white",
+    backgroundColor: "transparent",
+  });
+  gsap.to(".BURGERBAR", {
+    backgroundColor: "white",
+  });
+  gsap.to(".MOBILE-MENU", {
+    color: "white",
+    backgroundColor: "transparent",
+  });
+};
+
 const showAnim = gsap
   .from(".main-tool-bar", {
     yPercent: -100,
@@ -24,45 +55,61 @@ ScrollTrigger.create({
   onUpdate: (self) => {
     self.direction === -1 ? showAnim.play() : showAnim.reverse();
   },
-  onEnter: () => {
-    gsap.to(".navbar", { color: "black", backgroundColor: "white" });
-    gsap.to(".st0", { fill: "black" });
-    gsap.to(".st1", { fill: "#FF5B00" });
-  },
-  onLeaveBack: () => {
-    gsap.to(".navbar", { color: "white", backgroundColor: "transparent" });
-    gsap.to(".st0", { fill: "white" });
-    gsap.to(".st1", { fill: "white" });
-  },
+  onEnter: navbarToWhite,
+  onLeaveBack: navbarToTransparent,
 });
 
-const textAnimations = gsap.utils.toArray(".slide-text");
-textAnimations.forEach((text) => {
-  gsap.to(text, {
+// ----------------- burger menu MOBILE navbar ------------------
+
+const button = document.querySelector(".MOBILE-BUTTON");
+const menu = document.querySelector(".MOBILE-MENU");
+
+button.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (menu.classList.contains("opacity-0")) {
+    navbarToWhite();
+  } else {
+    navbarToTransparent();
+  }
+  menu.classList.toggle("opacity-0");
+}
+
+// --------------- SLIDING WELCOME on HERO Banner ---------------
+gsap.fromTo(
+  ".SLIDE-TEXT",
+  {
+    opacity: 0,
     y: -20,
-    duration: 0.8,
-    scrollTrigger: {
-      trigger: text,
-      toggleActions: "play none none none",
-    },
-  });
-});
-
-gsap.to(".image-zoom", {
-  scrollTrigger: {
-    trigger: ".image-zoom",
-    start: "50% bottom", // start: trigger, viewport
-    end: "bottom top", // end:   trigger, viewport
-    scrub: true,
-    // markers: true,
   },
-  scale: 1.3,
-  x: -50,
-  duration: 0.8,
-});
+  {
+    y: 0,
+    delay: 1,
+    opacity: 1,
+    duration: 0.8,
+  }
+);
 
-// experimenting with GSAP parallax - looks kind of choppy
+// const selectedText = document.querySelectorAll(".SLIDE-TEXT");
+// selectedText.forEach((text) => {
+//   text.classList.add("");
+// });
 
+// IMAGE ZOOM on SCROLL animation
+// gsap.to(".image-zoom", {
+//   scrollTrigger: {
+//     trigger: ".image-zoom",
+//     start: "50% bottom", // start: trigger, viewport
+//     end: "bottom top", // end:   trigger, viewport
+//     scrub: true,
+//     // markers: true,
+//   },
+//   scale: 1.3,
+//   x: -50,
+//   duration: 0.8,
+// });
+
+// experimenting with GSAP parallax - looks kind of choppy?
 // gsap.to(".parallax", {
 //   scrollTrigger: {
 //     start: "center center", // start: trigger, viewport
@@ -76,8 +123,8 @@ gsap.to(".image-zoom", {
 //   ease: "none",
 // });
 
-// TODO - make this work
-// TODO - remove @fullcalendar from the packages if not using them
+// TODO: - make this work
+// TODO: - remove @fullcalendar from the packages if not using them
 // https://sleepy-spire-45235.herokuapp.com/https://example.com (heroku site that should be proxy, not working)
 //
 // document.addEventListener("DOMContentLoaded", function () {
